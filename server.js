b1a9e9afa3b975cd711c830eac517fdc77f048f8
ASTELIE HENRYON (Etudiant) <henryast@ur.univ-rouen.fr>
2023-03-13 11:23:53 +0100

    ajout navbar dans index

--- server.js
+++ server.js
@@ -3,6 +3,10 @@ let app=express();
 let port=3000;
 
 app.use(express.static(__dirname+'/www'));
+app.use('/js',express.static(__dirname+'/node_modules/bootstrap/dist/js'));
+app.use('/js',express.static(__dirname+'/node_modules/jquey/dist'));
+app.use('/js',express.static(__dirname+'/node_modules/@popperjs/core/dist/umd'));
+app.use('/css',express.static(__dirname+'/node_modules/bootstrap/dist/css'));
 
 app.listen(port,()=>{
     console.log('Le serveur est en route');
@@ -11,4 +15,8 @@ app.listen(port,()=>{
 
 app.get('/',(req,res,next)=>{
     res.sendFile('/www/index.html');
+})
+
+app.get('/accueil',(req,res,next)=>{
+    res.sendFile('/www/accueil.html');
 })
