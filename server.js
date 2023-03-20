ff3cd9e57a44ff1e1fd164d8c34b86a6ad74ea69
Jyaax <julie.lcny@gmail.com>
2023-03-20 13:39:17 +0100

    premier commit

--- server.js
+++ server.js
@@ -7,14 +7,15 @@ app.use('/js',express.static(__dirname+'/node_modules/bootstrap/dist/js'));
 app.use('/js',express.static(__dirname+'/node_modules/jquey/dist'));
 app.use('/js',express.static(__dirname+'/node_modules/@popperjs/core/dist/umd'));
 app.use('/css',express.static(__dirname+'/node_modules/bootstrap/dist/css'));
-
+app.use('/views', express.static(__dirname + '/views'));
+app.use('view engine', 'ejs');
 app.listen(port,()=>{
     console.log('Le serveur est en route');
     console.log(`Serveur listening at http://localhost:${port}`);
 })
 
 app.get('/',(req,res,next)=>{
-    res.sendFile('/www/index.html');
+    res.render('/www/index.html');
 })
 
 app.get('/accueil',(req,res,next)=>{
