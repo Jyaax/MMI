2ee5d74c47173c6cb3046cfdce7b9b7a6e8a3c97
Jyaax <julie.lcny@gmail.com>
2023-03-20 14:15:37 +0100

    ajout de liens dans la navbar et ajout d'un objet monobjet aisni que la lecture de ses données

--- server.js
+++ server.js
@@ -9,13 +9,24 @@ app.use('/js',express.static(__dirname+'/node_modules/@popperjs/core/dist/umd'))
 app.use('/css',express.static(__dirname+'/node_modules/bootstrap/dist/css'));
 app.use('/views', express.static(__dirname + '/views'));
 app.set('view engine', 'ejs');
+
+let myobject={
+    nom: "Mon objet",
+    valeur: 10
+}
+
 app.listen(port,()=>{
     console.log('Le serveur est en route');
     console.log(`Serveur listening at http://localhost:${port}`);
 })
 
 app.get('/',(req,res,next)=>{
-    res.render('index.ejs');
+    res.render('index.ejs', {monobjet : myobject});
+})
+//page index
+
+app.get('/page2',(req,res,next)=>{
+    res.render('page2.ejs');
 })
 
 app.get('/accueil',(req,res,next)=>{
